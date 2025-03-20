9e01e57af2a5d1f1f2fae087554b80d86022f8a8
Rodrigo Ribeiro Carvalho <rodrigorc.dev@gmail.com>
2025-03-20 14:51:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,15 @@
-let pyodideReady = false;
-let pyodide;
-
-async function loadPyodideAndRun() {
-    if (!pyodideReady) {
-        pyodide = await loadPyodide();
-        pyodideReady = true;
-        console.log("Pyodide carregado com sucesso!");
-    }
-}
+let pyodideReadyPromise = loadPyodide();
 
 async function runPython() {
     let code = document.getElementById("code-editor").value;
     let outputElement = document.getElementById("output");
 
     try {
-        if (!pyodideReady) {
-            await loadPyodideAndRun();
-        }
-        let output = pyodide.runPython(code);
+        let pyodide = await pyodideReadyPromise;
+        let output = await pyodide.runPythonAsync(code);
         outputElement.textContent = output;
     } catch (error) {
         outputElement.textContent = "Erro ao executar o código: " + error;
         console.error(error);
     }
 }
-
-// Garante que o Pyodide carregue corretamente ao iniciar a página
-window.onload = async function() {
-    await loadPyodideAndRun();
-};
