e79c3266994cb26de2c628da068b658b428dd79b
Rodrigo Ribeiro Carvalho <rodrigorc.dev@gmail.com>
2025-03-20 14:42:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+let pyodideReady = false;
+let pyodide;
+
+async function loadPyodideAndRun() {
+    if (!pyodideReady) {
+        pyodide = await loadPyodide();
+        pyodideReady = true;
+    }
+}
+
 async function runPython() {
     let code = document.getElementById("code-editor").value;
     let outputElement = document.getElementById("output");
-    
+
     try {
-        let pyodide = await loadPyodide();
+        if (!pyodideReady) {
+            await loadPyodideAndRun();
+        }
         let output = pyodide.runPython(code);
         outputElement.textContent = output;
     } catch (error) {
         outputElement.textContent = "Erro: " + error;
     }
 }
+
+// Carregar o Pyodide uma única vez ao iniciar a página
+loadPyodideAndRun();
